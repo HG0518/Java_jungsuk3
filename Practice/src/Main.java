142d90a177035c8553cd54e527e860cfbdd58489
Lee Heegeon <hptlord@gmail.com>
2025-11-05 21:08:31 +0900

    3단원

--- Practice/src/Main.java
+++ Practice/src/Main.java
@@ -1,15 +1,8 @@
 import java.util.*;
 
 
-
 public class Main {
-    public static void main(String[] args)
-    {
-        long regNo = 98051810711112L;
-        System.out.println("regNo: " + regNo)  ;
-        System.out.println(abc);
+    public static void main(String[] args) {
+        
     }
-    
-    static double abc;
-
 }
