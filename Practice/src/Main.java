c266c1c770954481520d66e7b486b859b89a4779
Lee Heegeon <hptlord@gmail.com>
2025-11-12 19:09:26 +0900

    7강

--- Practice/src/Main.java
+++ Practice/src/Main.java
@@ -6,19 +6,14 @@ public class Main {
         Parent p = new Child();
         Child c = new Child();
 
-        System.out.println("p.x="+p.x);
         p.method();
-        System.out.println();
-        System.out.println("c.x="+c.x);
         c.method();
     }
 }
 
 class Parent{
     int x = 100;
-    void method(){
-        System.out.println("Parent Method");
-    }
+
 }
 
 class Child extends Parent{
